aed937d3547e754cb665213a0469c0fb93631fed
stepan-tsiolkovskiy <tsiolkovskiystepan@gmail.com>
2024-02-27 00:12:59 +0200

    test_commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
-import bookCatalogController from './book/routes/book.router.js';
-import { serveImage } from './image/controllers/image.controller.js';
-
 const app = express();
 
 app.use(cors({ origin: process.env.DEVELOP_STAGING }));
-
-app.use('/api', bookCatalogController);
-app.use('/api/images/:filename', serveImage);
 
 app.use('/api', (req, res) => {
   res.send('Welcome to the API!');
